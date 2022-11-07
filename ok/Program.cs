933de18cd75126e6d6335b1992625f675696be9c
John Doe <gustavo.galazo@etec.sp.gov.br>
2022-11-07 20:19:37 -0300

    teste da aula

---
+++ ok/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace definitivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            
+            Console.WriteLine("Escreva seu nome");
+            String nome;
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite seu endereço");
+            String endereço;
+            endereço = Console.ReadLine();
+           Console.WriteLine(endereço);
+            
+           Console.WriteLine("telefone");
+           String telefone;
+           telefone = Console.ReadLine();
+           Console.WriteLine(telefone);
+           Console.ReadLine(); 
+           
+
+
+
+
+        }
+    }
+}
